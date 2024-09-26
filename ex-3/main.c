5d7a1e5f5718c92c9e4e7baf01bf9a61971eeefc
FazonPlay <david.gevorgian69@gmail.com>
2024-09-26 11:29:36 +0200

    exercise 3

---
+++ ex-3/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{    
+    char * vanomobil = "david";
+    printf("Bonjour je suis %s\n", vanomobil );
+    exit (0);
+} 
