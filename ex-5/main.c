f0778664f9c40ff1f29d99974ddd8daaccb905ff
FazonPlay <david.gevorgian69@gmail.com>
2024-09-26 14:17:58 +0200

    exercise 5

---
+++ ex-5/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	double x = 7;
+	double y = 12;
+	double resultat = (x + y) / 2;
+	printf("La moyenne de 12 et 7 est egale a %f \n", resultat);
+	exit (0);
+
+}
