188e9ed95854a56480d17a51e302d8a43ebb1536
FazonPlay <david.gevorgian69@gmail.com>
2024-09-26 13:57:14 +0200

    exercise 4

---
+++ ex-4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int x = 14;
+	int y = 19;
+	int resultat = x * y;
+	printf("%d x %d = %d", 14, 19, x * y);
+	exit (0);
+}
